5b70f3e5554a5d3ed90ed1444e4db38ccdc41e26
Christian Ghione <cghione@gmail.com>
2022-05-24 15:14:34 -0300

    Cambios en detalles del Producto

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -14,12 +14,25 @@ class ProductResource extends JsonResource
      */
     public function toArray($request)
     {
+        // limito los campos que devuelvo en el JSON
         return [
             'name' => $this->name,
             'description' => $this->detail,
             'price' => $this->price,
-            'stock' => $this->stock,
-            'discount' => $this->discount
+            'stock' => $this->stock == 0 ? 'Sin Stock' : $this->stock,
+            'discount' => $this->discount,
+            // creo un campo con el precio final (precio*descuento)
+            'totalPrice' => round((1 - ($this->discount/100)) * $this->price,2),
+
+            // genero el rating del producto con las stars de los reviews
+            // si hay al menos 1 review hago el promedio
+            'rating' => $this->reviews->count() > 0
+                ? round( $this->reviews->sum('star') / $this->reviews->count(), 2 ) : 'Sin calificaciones.',
+
+            // genero el link a los reviws de ese producto
+            'href' => [
+                'reviews' => route('reviews.index', $this->id)
+            ]
         ];
     }
 }
